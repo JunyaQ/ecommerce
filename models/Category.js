29765cf78db0499427f83a17c30fc26493fef5ab
Junya Qiao <junyaqiao@gmail.com>
2021-12-26 21:00:15 -0500

    complete model category

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,20 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define columns, id and category_name
+    // Id: integer,not allow null, set primary key, auto increment
+    id:{
+      type:DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true,
+    },
+    //category name: string, not allow null
+    category_name:{
+      type:DataTypes.STRING,
+      allowNull:false,
+    }
+
   },
   {
     sequelize,
