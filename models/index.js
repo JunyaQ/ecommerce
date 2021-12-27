7be98bb86f60b0341f3458eb512d75f4873b86dd
Junya Qiao <junyaqiao@gmail.com>
2021-12-26 22:42:58 -0500

    create association mehtods

--- models/index.js
+++ models/index.js
@@ -3,14 +3,31 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsTo } = require('./Category');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey:'category_id',
+})
 
 // Categories have many Products
+Category.hasMany(Product,{
+  foreignKey:"category_id",
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through:ProductTag,
+  as:'product_id',
+  foreignKey:'product_id',
+})
+// Tags belongToMany Products (through ProductTag)(
+Tag.belongsToMany(Product,{
+  through:ProductTag,
+  as:'tag_id',
+  foreignKey:'tag_id',
 
-// Tags belongToMany Products (through ProductTag)
+})
 
 module.exports = {
   Product,
