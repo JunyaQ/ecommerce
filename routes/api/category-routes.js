79264902ddc884ecb0029ee2aa0a0466d52e46e1
Junya Qiao <junyaqiao@gmail.com>
2021-12-28 01:26:47 -0500

    category route find all

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,10 +2,23 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
+//id and category_name
 
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes:['id','category_name'],
+    include:[{
+      model:Product,
+      attributes:['id', 'product_name', 'price', 'stock', 'category_id'],
+    }]
+  })
+  .then(dbProductData=> res.json(dbProductData))
+  .catch(err=>{
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
